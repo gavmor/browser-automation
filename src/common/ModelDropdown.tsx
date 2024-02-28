e96904a4744375e859f964ac2709ca7312d25ffb
Gavin Morgan <morgan.gavin@gmail.com>
2024-02-27 20:20:27 -0800

    fetch ollama models

--- src/common/ModelDropdown.tsx
+++ src/common/ModelDropdown.tsx
@@ -1,5 +1,5 @@
 import { Select } from '@chakra-ui/react';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useAppState } from '../state/store';
 
 const ModelDropdown = () => {
@@ -7,16 +7,21 @@ const ModelDropdown = () => {
     selectedModel: state.settings.selectedModel,
     updateSettings: state.settings.actions.update,
   }));
+  const [models, setModels] = useState([])
+  useEffect(() => {
+    fetch('http://localhost:11434/api/tags', {})
+      .then(response => response.json())
+      .then(({models}) => setModels(models))
+  }, [])
 
   return (
     // Chakra UI Select component
     <Select
-      value={selectedModel || 'mistral'}
-      onChange={(e) => updateSettings({ selectedModel: e.target.value })}
-    >
-      <option value="mistral">Mistral</option>
-      <option value="mixtral">Mixtral</option>
-      <option value="neural-chat">Intel Neural-Chat</option>
+      value={selectedModel || ''}
+      onChange={(e) => updateSettings({ selectedModel: e.target.value })}>
+        {models.map(({name, model, digest}) => (
+          <option key={digest} value={model}>{name}</option>
+        ))}
     </Select>
   );
 };
